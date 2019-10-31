fc6313592b3ca2941816e2d15cc2335399a99f5c
Brad Winter <bradleyswinter@hotmail.co.uk>
2019-10-31 20:30:38 +0000

    Setup local dev server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,7 +7,7 @@ process.env.NODE_ENV = "development";
 module.exports = {
   mode: "development",
   target: "web",
-  devTool: "cheap-module-source-map",
+  devtool: "cheap-module-source-map",
   entry: "./src/index",
   output: {
     path: path.resolve(__dirname, "build"),
@@ -22,7 +22,7 @@ module.exports = {
     headers: { "Access-Control-Allow-Origin": "*" },
     https: false
   },
-  plugin: [
+  plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
       favicon: "src/favicon.ico"
