b7947dd130583b4978a0008983ad1ca0816cc806
Brad Winter <bradleyswinter@hotmail.co.uk>
2019-11-03 10:59:33 +0000

    Setup basic pages

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p>
+      THis app uses React, Redux and React Router, plus many other helpful
+      libraries.
+    </p>
+  </div>
+);
+
+export default AboutPage;
