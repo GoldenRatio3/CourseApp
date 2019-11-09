0287ed760a5de74a33c9bf5906c9ea0875229203
Brad Winter <bradleyswinter@hotmail.co.uk>
2019-11-09 22:30:19 +0000

    Added async spinner to courses page and slowed mock api

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,6 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
+import { beginApiCall } from "./apiStatusActions";
 
 export function loadCoursesSuccess(courses) {
   return {
@@ -21,6 +22,7 @@ export function updateCourseSuccess(course) {
 
 export function loadCourses() {
   return function(dispatch) {
+    dispatch(beginApiCall());
     return courseApi
       .getCourses()
       .then(courses => {
@@ -34,6 +36,7 @@ export function loadCourses() {
 
 export function saveCourse(course) {
   return function(dispatch, getState) {
+    dispatch(beginApiCall());
     return courseApi
       .saveCourse(course)
       .then(courses => {
