89a8b76492ab4421492d70adadf665592adc0ade
Brad Winter <bradleyswinter@hotmail.co.uk>
2019-11-10 08:52:09 +0000

    Added client side validation on add course form

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -37,6 +37,19 @@ function ManageCoursePage({
     }
   }, [props.course]);
 
+  function formIsValid() {
+    const { title, authorId, category } = course;
+    const errors = {};
+
+    if (!title) errors.title = "Title is required.";
+    if (!authorId) errors.author = "Author is required.";
+    if (!category) errors.category = "Category is required.";
+
+    setErrors(errors);
+    // Form is valid if the error object still has no properties
+    return Object.keys(errors).length === 0;
+  }
+
   function handleChange(event) {
     const { name, value } = event.target;
     setCourse(prevCourse => ({
@@ -47,6 +60,7 @@ function ManageCoursePage({
 
   function handleSave(event) {
     event.preventDefault();
+    if (!formIsValid()) return;
     setSaving(true);
     saveCourse(course)
       .then(() => {
